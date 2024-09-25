5e05bf50a63d726e83a4da67f52370b5a6c631b8
saif <kh.s.k.rohan@gmail.com>
2024-09-25 22:34:10 +0600

    history parital

--- scripts/events.js
+++ scripts/events.js
@@ -79,7 +79,6 @@ document.getElementById('donate-qoata-btn').addEventListener('click', function (
             document.getElementById('money-total').innerText = (totalMon - addMoney) + " BDT";
             document.getElementById('money-quata').innerText = (noaMon + addMoney) + " BDT";
             document.getElementById('donate-quata-input').value = "";
-
             modelShow();
         }
     }
@@ -102,3 +101,38 @@ document.getElementById('history-btn').addEventListener('click', function () {
     document.getElementById('donate-btn').style.backgroundColor = 'white';
     document.getElementById('history-btn').style.backgroundColor = '#B4F461';
 })
+
+
+document.getElementById('donate-noakhali-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const addMoney = parseFloat(document.getElementById('donate-noakhali-input').value);
+    console.log(addMoney);
+    if(addMoney>0){
+        console.log('hello');
+        
+        const bap = document.getElementById('history-container');
+
+        const div = document.createElement('div');
+        div.classList.add('border', 'border-[1px]', 'rounded-2xl', 'p-8', 'mb-6');
+
+        const para = document.createElement('p');
+        para.classList.add('font-bold', 'text-black', 'mb-4', 'text-base', 'md:text-xl');
+        para.innerText = addMoney + ' Taka is Donated for famine-2024 at Noakhali, Bangladesh';
+
+        div.appendChild(para);
+
+        const dateToday = new Date().toString();
+        const para2 = document.createElement('p');
+        para2.classList.add( 'text-sm', 'md:text-base', 'text-black', 'text-opacity-75');
+        para2.innerText = 'Date: ' +   dateToday;
+
+        
+        div.appendChild(para2);
+        bap.append(div);
+
+    }
+    else{
+        console.log('rohan')
+    }
+});
